aaded77cad7be68bcd54ce8d4907607ba5ecd327
atul-7 <akohar007@gmail.com>
2021-06-29 23:12:31 +0530

    hello

--- CSES Problem Set/Introductory Problems/TwoKnights.cpp
+++ CSES Problem Set/Introductory Problems/TwoKnights.cpp
@@ -26,11 +26,37 @@ using namespace std;
 int main()
 {
     fast_io;
-    int n;
+    ll n;
     cin >> n;
-    for (ll i = 1; i <= n; i++)
+    ll tot = n * (n + 1) / 2;
+    if (tot % 2)
     {
-        ll ans = i * i * (i * i - 1) / 2 - 4 * (i - 1) * (i - 2);
-        cout << ans << "\n";
+        cout << "NO\n";
+        return 0;
     }
+    tot /= 2;
+    vector<int> a1, a2;
+    for (ll i = n, sum = 0; i > 0; i--)
+    {
+        if (sum + i <= tot)
+        {
+            sum += i;
+            a1.push_back(i);
+        }
+        else
+        {
+            a2.push_back(i);
+        }
+    }
+    sort(a1.begin(), a1.end());
+    sort(a2.begin(), a2.end());
+    cout << "YES\n";
+    cout << a1.size() << "\n";
+    for (int x : a1)
+        cout << x << " ";
+    cout << "\n";
+    cout << a2.size() << "\n";
+    for (int x : a2)
+        cout << x << " ";
+    cout << "\n";
 }
